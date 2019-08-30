da02eec9122fdf2e78f5fd25bbd2d3aa40f257ae
Jakub Fides <jakub.fides@gmail.com>
2019-08-30 14:20:05 +0200

    hover

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import "./menu-item.scss";
 
-const MenuItem = ({ title, imageUrl }) => (
-  <div
-    style={{
-      backgroundImage: `url(${imageUrl})`
-    }}
-    className="menu-item"
-  >
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    ></div>
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">Shop Now</span>
     </div>
   </div>
