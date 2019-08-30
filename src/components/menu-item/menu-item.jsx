59bc9dcab25b94058f396b7721369ff765d2b747
Jakub Fides <jakub.fides@gmail.com>
2019-08-30 13:25:22 +0200

    bg

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import "./menu-item.scss";
 
-const MenuItem = ({ title }) => (
-  <div className="menu-item">
+const MenuItem = ({ title, imageUrl }) => (
+  <div
+    style={{
+      backgroundImage: `url(${imageUrl})`
+    }}
+    className="menu-item"
+  >
     <div className="content">
       <h1 className="title">{title}</h1>
       <span className="subtitle">Shop Now</span>
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,41 +1,11 @@
 import React from "react";
 import "./homepage.styles.scss";
+import Directory from "./../../components/directory/directory";
 
 const HomePage = () => {
   return (
     <div className="homepage">
-      <div className="directory-menu">
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle">Shop Now</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">JACKETS</h1>
-            <span className="subtitle">Shop Now</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">SNEAKERS</h1>
-            <span className="subtitle">Shop Now</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">WOMENS</h1>
-            <span className="subtitle">Shop Now</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">MENS</h1>
-            <span className="subtitle">Shop Now</span>
-          </div>
-        </div>
-      </div>
+      <Directory />
     </div>
   );
 };
