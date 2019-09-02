a1fe44e792f790f02c9b917388b4133dc32db478
Jakub Fides <jakub.fides@gmail.com>
2019-09-02 16:15:13 +0200

    router

--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -47,10 +47,26 @@ export default class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
   }
+
+  // render() {
+  //   return (
+  //     <div className="directory-menu">
+  //       {this.state.sections.map(({ title, imageUrl, id, size, linkUrl }) => (
+  //         <MenuItem
+  //           key={id}
+  //           title={title}
+  //           imageUrl={imageUrl}
+  //           size={size}
+  //           linkUrl={linkUrl}
+  //         />
+  //       ))}
+  //     </div>
+  //   );
+  // }
 }
