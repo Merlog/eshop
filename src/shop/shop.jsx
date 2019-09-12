0b20278c6a99273920838e184b3d3b5e91991992
Jakub Fides <jakub.fides@gmail.com>
2019-09-12 15:16:56 +0200

    collection item

--- src/shop/shop.jsx
+++ src/shop/shop.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import SHOP_DATA from "./shop.data.js";
-
+import CollectionPreview from "./../components/collection-preview/collection-preview";
 class ShopPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      colections: SHOP_DATA
+      collections: SHOP_DATA
     };
   }
 
   render() {
-    return <div>SHOP PAGE</div>;
+    const { collections } = this.state;
+    return (
+      <div className="shop=page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
   }
 }
 
