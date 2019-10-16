c717fcb385ed2627c4f13776f2d02d614c4bcb46
Jakub Fides <jakub.fides@gmail.com>
2019-10-16 09:18:22 +0200

    bookmark

---
+++ bookmark2.js
@@ -0,0 +1,14 @@
+
+
+Global
+
+  https://1stwebdesigner.com/essential-cheatsheets-web-design-developer/
+
+
+CSS
+
+  Slovnik
+    http://apps.workflower.fi/vocabs/css/en#id-selector
+
+  Jednotky
+    https://www.vzhurudolu.cz/prirucka/css3-jednotky
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { setCurrentUser } from "./redux/user/user.actions";
 
@@ -43,18 +43,22 @@ class App extends React.Component {
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
           <Route path="/contact" component={ShopPage} />
-          <Route path="/signin" component={SignInUp} />
+          <Route exact path="/signin" render={()=> this.props.currentUser ? (<Redirect to='/'/>) : (<SignInUp />)} />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(App);
